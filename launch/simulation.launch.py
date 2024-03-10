3459bc0cc1efb21ca03a1710f828888de5495bc4
DWxPro <daniel-wengeler@gmx.de>
2024-03-10 23:43:50 +0100

    adding nav2 localization

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -20,34 +20,40 @@ from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
 
+from nav2_common.launch import RewrittenYaml
+from launch_ros.descriptions import ComposableNode, ParameterFile
 
+from launch.substitutions import TextSubstitution
+from launch.actions import LogInfo
+
+import yaml
 
 def generate_launch_description():
 
-    package_name = 'vacuum_cleaner'
-    urdf_name = 'robot.urdf.xacro'
-    urdf_mappings = {'sim_mode': "true"}
-    world_file_name = 'my_world.world'
-    slam_mapping = True
-    slam_mapping_file_name = 'slam_mapping.yaml'
-    slam_localization_file_name = 'slam_localization.yaml'
-    rviz_file_name = 'presettings.rviz'
-    joystick_file_name = 'joystick.yaml'
+    use_sim_time        = True
+    package_name        = 'vacuum_cleaner'
+    urdf_name           = 'robot.urdf.xacro'
+    world_file_name     = 'my_world.world'
+    slam_file_name      = 'slam.yaml'
+    rviz_file_name      = 'presettings.rviz'
+    joystick_file_name  = 'joystick.yaml'
     twist_mux_file_name = 'twist_mux.yaml'
+    map_file_name       = "my_map.yaml"
+    nav2_file_name      = 'nav2.yaml'
 
-    path_package_share = FindPackageShare(package=package_name).find(package_name)
-    path_gazebo_ros_share = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
+    path_package_share      = FindPackageShare(package=package_name).find(package_name)
+    path_gazebo_ros_share   = FindPackageShare(package='gazebo_ros').find('gazebo_ros')
 
-    robot_description = xacro.process_file(os.path.join(path_package_share,'description',urdf_name), mappings = urdf_mappings).toxml()
-    rviz_settings = os.path.join(path_package_share,'config',rviz_file_name)
-    if slam_mapping:
-        slam_settings = os.path.join(path_package_share,'config',slam_mapping_file_name)
-    else:
-        slam_settings = os.path.join(path_package_share,'config',slam_localization_file_name)
-    joystick_settings = os.path.join(path_package_share ,'config',joystick_file_name)
-    twist_mux_settings = os.path.join(path_package_share,'config',twist_mux_file_name)
+    use_sim_time_text   = "true" if use_sim_time else "false"
 
-    world_settings = os.path.join(path_package_share, 'worlds', world_file_name)
+    robot_description   = xacro.process_file(os.path.join(path_package_share,'description',urdf_name), mappings = {'sim_mode': use_sim_time_text}).toxml()
+    rviz_settings       = os.path.join(path_package_share,'config',rviz_file_name)
+    slam_settings       = os.path.join(path_package_share,'config',slam_file_name)
+    joystick_settings   = os.path.join(path_package_share ,'config',joystick_file_name)
+    twist_mux_settings  = os.path.join(path_package_share,'config',twist_mux_file_name)
+    world_settings      = os.path.join(path_package_share, 'worlds', world_file_name)
+    map_file            = os.path.join(path_package_share,'maps',map_file_name)
+    nav2_settings_raw   = os.path.join(path_package_share,'config',nav2_file_name)
 
     # controller_manager
     # running inside gazebo
@@ -80,7 +86,7 @@ def generate_launch_description():
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description, 'use_sim_time': True}]
+        parameters=[{'robot_description': robot_description, 'use_sim_time': use_sim_time}]
     )
     
     # visualize robot
@@ -96,22 +102,64 @@ def generate_launch_description():
     slam_params_file = LaunchConfiguration('slam_params_file')
     
     declare_slam_parameters = DeclareLaunchArgument(
-        'slam_params_file', default_value=slam_settings)
+        'slam_params_file', default_value = slam_settings)
 
     slam_toolbox_node = LifecycleNode(
-        parameters=[slam_params_file,{'use_sim_time': True}],
+        parameters=[slam_params_file,{'use_sim_time': use_sim_time}],
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen',
         namespace=''
     )
-       
+    
+    # localication
+    param_substitutions = {
+        'use_sim_time': use_sim_time_text,
+        'yaml_filename': map_file}
+
+    nav2_settings = ParameterFile(
+        RewrittenYaml(
+            source_file=nav2_settings_raw,
+            param_rewrites=param_substitutions,
+            convert_types=True),
+        allow_substs=True)
+
+    nav2_lifecycle_manager_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time},
+                    {'autostart': True},
+                    {'node_names': ['map_server', 'amcl']}])
+
+    nav2_map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        respawn_delay=2.0,
+        parameters=[nav2_settings],
+        remappings=[('/tf', 'tf'),('/tf_static', 'tf_static')])
+    
+    nav2_amcl_node = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        respawn_delay=2.0,
+        parameters=[nav2_settings],
+        remappings=[('/tf', 'tf'),('/tf_static', 'tf_static')])
+
+    # naviation
+
+
     # twist_mux
     twist_mux_node = Node(
             package="twist_mux",
             executable="twist_mux",
-            parameters=[twist_mux_settings, {'use_sim_time': True}],
+            parameters=[twist_mux_settings, {'use_sim_time': use_sim_time}],
             remappings=[('/cmd_vel_out','/diffbot_base_controller/cmd_vel_unstamped')]
     )
     
@@ -119,14 +167,14 @@ def generate_launch_description():
     joystick_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joystick_settings, {'use_sim_time': True}]
+            parameters=[joystick_settings, {'use_sim_time': use_sim_time}]
     )
 
     teleop_twist_joy_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
-            parameters=[joystick_settings, {'use_sim_time': True}],
+            parameters=[joystick_settings, {'use_sim_time': use_sim_time}],
             remappings=[('/cmd_vel','/cmd_vel_joy')]
     )
 
@@ -161,11 +209,16 @@ def generate_launch_description():
     ld.add_action(diffbot_base_controller_spawner)
     ld.add_action(joint_state_broadcaster_spawner)
     ld.add_action(sweeper_controller_spawner)
-    ld.add_action(declare_slam_parameters)
-    ld.add_action(slam_toolbox_node)
+    #ld.add_action(declare_slam_parameters)
+    #ld.add_action(slam_toolbox_node)
     ld.add_action(rviz_node)
     ld.add_action(twist_mux_node)
     ld.add_action(joystick_node)
-    ld.add_action(teleop_twist_joy_node)
+    ld.add_action(teleop_twist_joy_node) 
+    ld.add_action(nav2_lifecycle_manager_node)
+    ld.add_action(nav2_map_server_node)
+    ld.add_action(nav2_amcl_node)
 
     return ld
+
+
